a93a7ace57dca0a036ec9646dce996d94991f775
Klee Collage <klee.collage@gmail.com>
2025-01-21 17:26:59 -0600

    Validations

--- src/main/java/beans/backing/VacantForm.java
+++ src/main/java/beans/backing/VacantForm.java
@@ -2,6 +2,8 @@ package beans.backing;
 
 import beans.model.Candidate;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.apache.logging.log4j.LogManager;
@@ -15,12 +17,24 @@ public class VacantForm {
 
     Logger log = LogManager.getLogger(VacantForm.class);
 
+    public VacantForm() {
+        log.info("Created object VacantForm");
+    }
+
     public void setCandidate(Candidate candidate) {
         this.candidate = candidate;
     }
 
     public String send() {
         if (candidate.getName().equals("John")) {
+            if (this.candidate.getSurname().equals("Doe")) {
+                String msg = "Thank you but John Doe already work with us";
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                String componentId = null;
+                facesContext.addMessage(componentId, facesMessage);
+                return "index";
+            }
             log.info("Enter success case");
             return "success";
         }
--- src/main/java/beans/model/Candidate.java
+++ src/main/java/beans/model/Candidate.java
@@ -8,9 +8,16 @@ import org.apache.logging.log4j.Logger;
 @Named
 @RequestScoped
 public class Candidate {
-    private String name = "Enter your name";
+    private String name;
+    private String surname;
+    private String desiredSalary;
 
     Logger log = LogManager.getRootLogger();
+
+    public Candidate() {
+        log.info("Object Candidate created");
+        this.setName("Enter your name");
+    }
 
     public String getName() {
         return name;
@@ -19,5 +26,23 @@ public class Candidate {
     public void setName(String name) {
         this.name = name;
         log.info("Modify name property: " + this.name);
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+        log.info("Modify surname property: " + this.surname);
+    }
+
+    public String getDesiredSalary() {
+        return desiredSalary;
+    }
+
+    public void setDesiredSalary(String desiredSalary) {
+        this.desiredSalary = desiredSalary;
+        log.info("Modify desired salary property: " + this.desiredSalary);
     }
 }
